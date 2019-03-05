baaa472e6dfbb4b281aea644894091890991a8a9
Igor Chernyshov <ichernyshov@bk.ru>
2019-03-05 19:46:31 +0300

    Added custom Observable class and Observer

---
+++ millionaire/App/Entity/Observer.swift
@@ -0,0 +1,14 @@
+//
+//  Observer.swift
+//  millionaire
+//
+//  Created by Igor Chernyshov on 05/03/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import Foundation
+
+/// This class will observe custom Observable objects. It doesn't have to do something specific.
+class Observer {
+  
+}
