a0c7b4f405c2794f03f0d248928c8e30a738f487
Igor Chernyshov <ichernyshov@bk.ru>
2019-03-05 14:50:25 +0300

    Added OptionsVC

---
+++ millionaire/App/Flows/OptionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OptionsViewController.swift
+//  millionaire
+//
+//  Created by Igor Chernyshov on 05/03/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import UIKit
+
+/// This view controller allows user to change various game options that are stored in Game singleton.
+final class OptionsViewController: UIViewController {
+  
+  
+  
+}
